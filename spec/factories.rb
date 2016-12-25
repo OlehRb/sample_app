7e6a009866227410b01cf4da16eb4c05f5b805f1
OlehRb <qew1397@gmail.com>
2016-12-25 20:35:08 +0200

    Finish user sign up

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "OLeh RIabychev"
+    email    "qew1397@gmail.com"
+    password "qewqew1234"
+    password_confirmation "qewqew1234"
+  end
+end
